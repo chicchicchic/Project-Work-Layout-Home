09123bb7f83bdec55d88cce647ec96352c27fea3
chicchicchic <chicken481999@gmail.com>
2022-10-07 19:14:47 +0700

    7:13PM  07/10/2022

--- src/js/responsiveNavbar.js
+++
@@ -1,9 +0,0 @@
-// let hasMenubarMobile = $('#menu-bars');
-// if(hasMenubarMobile) {
-//     $(document).ready(function(){
-//         $('#menu-bars').click(function(){
-//             $('.navbar').toggleClass('active');
-//         });
-    
-//     });
-// }
